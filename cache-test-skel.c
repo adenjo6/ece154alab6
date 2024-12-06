0e10bba31815925c60e9ba4097e62afcf1712a78
Aden <adenjo@ucsb.edu>
2024-12-05 16:05:00 -0800

    Added

--- cache-test-skel.c
+++ cache-test-skel.c
@@ -1,95 +1,66 @@
-/*
-Aden Jo, Leo Zhu
-ECE 154A - Fall 2024
-Lab 2 - Mystery Caches
-Due: 12/5/2024
-
-Mystery Cache Geometries:
-mystery0:
-    block size = 
-    cache size = 
-    associativity = 
-mystery1:
-    block size = 
-    cache size = 
-    associativity = 
-mystery2:
-    block size = 
-    cache size = 
-    associativity = 
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "mystery-cache.h"
 
-/* 
+/*
    Returns the size (in B) of the cache
 */
 int get_cache_size(int block_size) {
-    int num_blocks = 1;
-
-    while (TRUE) {
+  int blocks = 0;
+  
+  flush_cache();
+    while(TRUE){        
         flush_cache();
-
-        for (int i = 0; i < num_blocks; i++) {
-            access_cache(i * block_size);
+        access_cache(0);
+        int i = 1;
+        while ( i <= blocks ){
+            access_cache(i*block_size);
+            i++;
         }
-
-        if (!access_cache(0)) {
-            return (num_blocks - 1) * block_size;
+        if ( access_cache(0) ){
+            blocks++; 
         }
-
-        num_blocks++;
+        else {
+            break;
+        }        
     }
-
-    return -1; 
+  return blocks*block_size;
 }
 
 /*
    Returns the associativity of the cache
 */
-int get_cache_assoc(int cache_size) {
-    int assoc = 1;
-    addr_t test_addr = 0;
-    int block_size = get_block_size();
-
-    while (TRUE) {
-        flush_cache();
-
-        for (int i = 0; i < assoc; i++) {
-            access_cache(test_addr + i * cache_size);
-        }
-
-        if (!access_cache(test_addr)) {
-            return assoc - 1;
-        }
-
-        assoc++;
-
-        if (assoc > 64) {
-            break; 
-        }
+int get_cache_assoc(int size) {
+  int assoc = 1;
+  flush_cache();
+  access_cache(0);
+  while (access_cache(0)){
+    flush_cache();
+    access_cache(0);
+    int i = 1;
+    while (i<=assoc){
+      access_cache(i*size);
+      i++;
     }
-
-    return -1; // Should not reach here
+    assoc++;
+  }
+  return assoc-1;
 }
 
 /*
    Returns the size (in B) of each block in the cache.
 */
 int get_block_size() {
-    flush_cache();
-    addr_t base_addr = 0;
-    access_cache(base_addr);
-    int block_size = 1;
+  
+  int block_size = 1;
+  flush_cache();
+  access_cache(0);
+  while (access_cache(block_size)) {
+      block_size++;
+  }
 
-    while (access_cache(base_addr + block_size)) {
-        block_size++;
-    }
-
-    return block_size;
+  return block_size;
 }
 
 int main(void) {
